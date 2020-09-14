c8dce0436a8acdef3c454f73242a960c97da91e3
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2020-09-15 00:47:09 +0530

    carousel issue fixed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavbarComp from "./Components/NavbarComp";
 import SideNav from "./Components/SideNav";
-import Carousel1 from "./Homepage/Carousel1";
 import Carousel from "./Components/Carousel/Carousel";
 
 import img1 from "./Homepage/Images/img1.jpg";
 import img2 from "./Homepage/Images/img2.jpg";
 import img3 from "./Homepage/Images/img3.jpg";
 
+const images = [img1, img2, img3];
 
 class App extends Component {
 	constructor(props) {
