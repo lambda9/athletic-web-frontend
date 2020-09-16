fe5ccff0a283e1009f7dc5544d1d6f126edebe4e
Devendra Singh <sdev6631@gmail.com>
2020-09-16 16:01:53 +0530

    title bar design updated and card shadow added

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import './TitleCSS.css'
+import React from "react";
+import "./TitleCSS.css";
 
 function Title(props) {
-    return (
-        <div className='title'>
-            <h4>{props.children}</h4>
-            <div />
-        </div>
-    )
+  return (
+    <div className="title">
+      <h4>{props.children}</h4>
+      <div className="baseline">
+        {" "}
+        <div className="title-mask title-mask1"></div>
+        <div className="title-mask title-mask2"></div>
+        <div className="title-mask title-mask3"></div>
+        <div className="title-mask title-mask4"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Title
+export default Title;
