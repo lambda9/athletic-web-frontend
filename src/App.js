9f04d4cca7159426499a5189d6902efcd930642b
Devendra Singh <sdev6631@gmail.com>
2020-09-18 13:57:16 +0530

    Changes in import format

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Pages/Home'
-import OurPrograms from './Components/Pages/OurPrograms'
-import ContactUs from './Components/Pages/ContactUs'
-import AboutUs from './Components/Pages/AboutUs'
-import FAQ from './Components/Pages/FAQ'
-import Gallery from './Components/Pages/Gallery'
-import Membership from './Components/Pages/Membership'
-import Error from './Components/Pages/Error'
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Pages/Home";
+import OurPrograms from "./Components/Pages/OurPrograms";
+import ContactUs from "./Components/Pages/ContactUs";
+import AboutUs from "./Components/Pages/AboutUs";
+import FAQ from "./Components/Pages/FAQ";
+import Gallery from "./Components/Pages/Gallery";
+import Membership from "./Components/Pages/Membership";
+import Error from "./Components/Pages/Error";
 
 class App extends Component {
   render() {
@@ -19,13 +19,13 @@ class App extends Component {
         <Switch>
           <Route exact path="/h" component={Home} />
           <Route exact path="/programs/" component={OurPrograms} />
-		  <Route exact path="/contactUs/" component={ContactUs} />
-		  <Route exact path="/aboutUs/" component={AboutUs} />
-		  <Route exact path="/faq/" component={FAQ} />
-		  <Route exact path="/gallery/" component={Gallery} />
-		  <Route exact path="/membership/" component={Membership} />
+          <Route exact path="/contactUs/" component={ContactUs} />
+          <Route exact path="/aboutUs/" component={AboutUs} />
+          <Route exact path="/faq/" component={FAQ} />
+          <Route exact path="/gallery/" component={Gallery} />
+          <Route exact path="/membership/" component={Membership} />
           <Route component={Error} />
-		</Switch>
+        </Switch>
       </Router>
     );
   }
