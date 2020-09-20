8d673b587a59992fc6e39dd7f02ba1ecbd697c55
Devendra Singh <sdev6631@gmail.com>
2020-09-21 00:55:07 +0530

    Fix hero.js to change bg-images dynamically

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,8 @@
 import React from "react";
-import img from '../Images/im6.jpg'
-
 
 function Hero(props) {
-  let tt = "banner-bg ";
-  let bg = {
-    backgroundImages: `url(${img}) center/cover no-repeat`
-  }
-
-  let bannerBg = {
-    minHeight: "calc(55vh - 66px)",
-    background: `url(${img}) center/cover no-repeat`,
-    backgroundColor: "lavender",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    transition: "var(--mainTransition)"
-  }
-  
-  return <div style={bannerBg}>{props.children}</div>;
+  let tt = "banner-bg " + props.tempClass;
+  return <div className={tt}>{props.children}</div>;
 }
 
 export default Hero;
