406668a4934e92ca1d441ab6167f04542291e39a
Devendra Singh <sdev6631@gmail.com>
2020-09-20 19:36:52 +0530

    hero and banner in progress

--- src/Components/Shapes/Circle.js
+++ src/Components/Shapes/Circle.js
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Circle(props) {
-    
-    let style={
-        position: "absolute",
-        top: `${props.top}rem`,
-        left: `${props.left}rem`,
-        width: `${props.r1}px`,
-        height: `${props.r2}px`,
-        backgroundColor: `${props.color}`,
-        borderRadius: "50%"
-    }
+  let style = {
+    position: "absolute",
+    top: `${props.top}rem`,
+    left: `${props.left}rem`,
+    width: `${props.r1}px`,
+    height: `${props.r2}px`,
+    backgroundColor: `${props.color}`,
+    borderRadius: "50%",
+  };
 
-    return (
-        <div style={style}/>
-    )
+  return <div style={style} />;
 }
 
-export default Circle
+export default Circle;
--- src/Components/Shapes/Square.js
+++ src/Components/Shapes/Square.js
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Square(props) {
+  let style = {
+    position: "absolute",
+    top: `${props.top}rem`,
+    left: `${props.left}rem`,
+    width: `${props.width}px`,
+    height: `${props.height}px`,
+    backgroundColor: `${props.color}`,
+    transform: `rotate(${props.angle}deg)`,
+  };
 
-    let style={
-        position: "absolute",
-        top: `${props.top}rem`,
-        left: `${props.left}rem`,
-        width: `${props.width}px`,
-        height: `${props.height}px`,
-        backgroundColor: `${props.color}`,
-        transform: `rotate(${props.angle}deg)`,
-    }
-
-    return (
-        <div style={style}/>
-    )
+  return <div style={style} />;
 }
 
-export default Square
+export default Square;
