574ff7d043023624feba9b2ad1b9587e05ce6c20
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2020-09-24 00:42:21 +0530

    bottom navbar scroll added on small height

--- src/Components/Navbar/BottomNav.js
+++ src/Components/Navbar/BottomNav.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
@@ -12,27 +12,48 @@ const BottomNav = ({ visible }) => {
 		["contactUs", "contact us"],
 	];
 
+	const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+	const [overflowHeight, setOverflowHeight] = useState(visible ? 300 : 0);
+
+	const onWindowResize = () => {
+		console.log("height", window.innerHeight);
+		setWindowHeight(window.innerHeight);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", onWindowResize);
+		if (windowHeight <= 370 && visible) {
+			setOverflowHeight(windowHeight - 70);
+		} else {
+			setOverflowHeight(300);
+		}
+		return () => {
+			window.removeEventListener("resize", onWindowResize);
+		};
+	});
+
 	return (
 		<div
-			className={visible ? "bottom-nav bottom-nav-open" : "bottom-nav"}
+			className={"bottom-nav"}
 			style={{
-				width: "100%",
-				overflow: "hidden",
+				height: `${visible ? overflowHeight : 0}px`,
 			}}
 		>
 			<div className="bottom-nav-link-container">
 				{links.map((value, index) => {
 					return (
-						<Fragment key={index}>
-							<Link to={value[0]}>{value[1]}</Link>
-							<div
-								style={{
-									width: "100%",
-									height: "2px",
-									backgroundColor: "#459fb6",
-								}}
-							></div>
-						</Fragment>
+						// <Fragment key={index}>
+						<Link key={index} to={value[0]}>
+							{value[1]}
+						</Link>
+						// <div
+						// 	style={{
+						// 		width: "100%",
+						// 		height: "2px",
+						// 		backgroundColor: "#459fb6",
+						// 	}}
+						// ></div>
+						// </Fragment>
 					);
 				})}
 			</div>
