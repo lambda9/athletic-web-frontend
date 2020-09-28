10c09ae25985ccb1172758518d479b1b0a953717
Devendra Singh <sdev6631@gmail.com>
2020-09-28 16:27:05 +0530

    Add Our team component to About Us page and Update css for title and footer.

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -3,12 +3,13 @@ import "./AboutUs.css";
 import Hero from "../Hero";
 import AboutUsBg from "./AboutUsBg";
 import AboutCard from "./AboutCard";
-import Services from '../Services/Services'
+import Services from "../Services/Services";
 import img1 from "../../Images/im20.jpg";
 import img2 from "../../Images/im21.jpg";
 import img3 from "../../Images/im10.jpg";
 import backDropImg from "../../Images/im5.jpg";
 import OurValues from "./OurValues";
+import Title from "../Title/Title";
 
 const desc =
   " We want to help you live a fit and healthy lifestyle! We do this by helping you find the most suitable equipment for your home gym, studio or commercial gym, keeping your budget, lifestyle and fitness goals in mind.";
@@ -45,6 +46,24 @@ class AboutUs extends Component {
         </div>
 
         <OurValues />
+
+        <div className="about-our-team-title">
+          <Title>our team</Title>
+        </div>
+        <div className="about-content">
+          <AboutCard
+            img={img1}
+            title="Swaroop Saran"
+            description="my name is swaroop saran. Im the owner"
+            inverted={false}
+          />
+          <AboutCard
+            img={img2}
+            title="Bhati"
+            description="my name is bhati. Im the trainer of our gym"
+            inverted={true}
+          />
+        </div>
       </div>
     );
   }
--- src/Components/AboutUs/OurValues.js
+++ src/Components/AboutUs/OurValues.js
@@ -11,25 +11,25 @@ import "../Services/Services.css";
 function OurValues() {
   const services = [
     {
-      icon: <RiTeamFill className='services-item-icon'/>,
+      icon: <RiTeamFill className="services-item-icon" />,
       title: "Team Work",
       info:
         "We are committed to common goals. Effective communication and accountability makes our team achieve greater results.",
     },
     {
-      icon: <BsGearWideConnected className='services-item-icon' />,
+      icon: <BsGearWideConnected className="services-item-icon" />,
       title: "Innovation",
       info: "We train smart not hard with our best techinques and innovation.",
     },
 
     {
-      icon: <BsFlagFill className='services-item-icon'/>,
+      icon: <BsFlagFill className="services-item-icon" />,
       title: "Be the change",
       info:
         "We believe that we should all take ownership and have the courage to lead change.",
     },
     {
-      icon: <MdHeadsetMic className='services-item-icon'/>,
+      icon: <MdHeadsetMic className="services-item-icon" />,
       title: "Customer Service",
       info:
         "Customers are our priority. We are passionate about delivering exceptional, personalised customer service to all.",
@@ -44,8 +44,8 @@ function OurValues() {
           return (
             <div key={item.title} className="services-item-container">
               {item.icon}
-              <div className='services-item-card-border1'/>
-              <div className='services-item-card-border2'/>
+              <div className="services-item-card-border1" />
+              <div className="services-item-card-border2" />
 
               <h4>{item.title}</h4>
               <p>{item.info}</p>
