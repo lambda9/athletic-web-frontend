5b2c8133fabbd45c7041fcc3d2a86f21dd06e5c4
bob <vikas.singh.iitr@gmail.com>
2020-09-30 19:20:33 +0530

    Add EntryForm in Contact us page

---
+++ src/Components/ContactUs/ContactUs.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Hero from "../Hero";
+import bgImg from "../../Images/im9.jpg";
+import EntryForm from './EntryForm'
+
+class ContactUs extends Component {
+  render() {
+    return (
+      <div>
+        <Hero
+          bgImg={bgImg}
+          title="Contact us"
+          description="Mail, call, text or meet on-site"
+        ></Hero>
+
+        <EntryForm />
+      </div>
+    );
+  }
+}
+
+export default ContactUs;
