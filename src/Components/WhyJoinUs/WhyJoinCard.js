6c1c049c2ff8c0f1a787d318c7dd17ccac68022b
Devendra Singh <sdev6631@gmail.com>
2020-10-06 21:11:00 +0530

    Update restricted motion to floating ball

--- src/Components/WhyJoinUs/WhyJoinCard.js
+++ src/Components/WhyJoinUs/WhyJoinCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./WhyJoinUs.css";
 import img1 from "../../Images/im8.jpg";
-import Title from '../Title/Title'
+import Title from "../Title/Title";
 import { Component } from "react";
 
 export class WhyJoinCard extends Component {
@@ -9,7 +9,7 @@ export class WhyJoinCard extends Component {
     super(props);
 
     this.state = {
-      x: 150,
+      x: 450,
       y: 200,
     };
   }
@@ -17,43 +17,61 @@ export class WhyJoinCard extends Component {
   componentDidMount() {
     this.element = document.getElementById("myDiv");
     this.circRad = 200;
-    this.dx = 3;
-    this.dy = 3;
+    this.dx = 0.5;
+    this.dy = 0.5;
     this.drawCirc();
   }
+
+  componentDidUpdate() {
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+
+  handleWindowResize = () => {
+    var tempWidth = window.innerWidth;
+    var tempHeight = window.innerHeight;
+
+    if (this.x > tempWidth) {
+      this.setState({
+        x: tempWidth,
+      });
+    }
+
+    if (this.y > tempHeight) {
+      this.setState({
+        y: tempHeight,
+      });
+    }
+  };
 
   drawCirc = () => {
     if (
       this.state.x < 0 ||
-      this.state.x > this.element.offsetHeight - this.circRad
+      this.state.x > this.element.offsetWidth - this.circRad
     ) {
       this.dx = -this.dx;
     }
     if (
       this.state.y < 0 ||
-      this.state.y > this.element.offsetWidth - this.circRad
+      this.state.y > this.element.offsetHeight - this.circRad
     ) {
       this.dy = -this.dy;
     }
 
     this.setState({
-      x: this.state.x + this.dx,
-      y: this.state.y + this.dy
+      //   x: this.state.x + this.dx,
+      y: this.state.y + this.dy,
     });
     window.requestAnimationFrame(this.drawCirc);
   };
 
   render() {
-    console.log("im rendering");
-
     return (
       <div className="wju-main-div" id="myDiv">
         <div
           className="wju-card"
-          
           style={{
-            top: `${this.state.x}px`,
-            left: `${this.state.y}px`,
+            top: `${this.state.y}px`,
+            left: `${this.state.x}px`,
           }}
         >
           <img src={img1}></img>
