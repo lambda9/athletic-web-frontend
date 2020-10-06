8b48cdca29d3fbf3f48328b237fa0f83f91c6687
Devendra Singh <sdev6631@gmail.com>
2020-10-06 21:44:53 +0530

    Add multiballs and fix home page gap above our plans issue

--- src/Components/WhyJoinUs/WhyJoinCard.js
+++ src/Components/WhyJoinUs/WhyJoinCard.js
@@ -9,16 +9,16 @@ export class WhyJoinCard extends Component {
     super(props);
 
     this.state = {
-      x: 450,
-      y: 200,
+      x: this.props.x,
+      y: this.props.y,
     };
   }
 
   componentDidMount() {
-    this.element = document.getElementById("myDiv");
+    // this.element = document.getElementById("myDiv");
     this.circRad = 200;
-    this.dx = 0.5;
-    this.dy = 0.5;
+    this.dx = 1;
+    this.dy = 1;
     this.drawCirc();
   }
 
@@ -44,15 +44,15 @@ export class WhyJoinCard extends Component {
   };
 
   drawCirc = () => {
-    if (
-      this.state.x < 0 ||
-      this.state.x > this.element.offsetWidth - this.circRad
-    ) {
-      this.dx = -this.dx;
-    }
+    // if (
+    //   this.state.x < 0 ||
+    //   this.state.x > this.element.offsetWidth - this.circRad
+    // ) {
+    //   this.dx = -this.dx;
+    // }
     if (
       this.state.y < 0 ||
-      this.state.y > this.element.offsetHeight - this.circRad
+      this.state.y > 600 - this.circRad
     ) {
       this.dy = -this.dy;
     }
@@ -66,7 +66,6 @@ export class WhyJoinCard extends Component {
 
   render() {
     return (
-      <div className="wju-main-div" id="myDiv">
         <div
           className="wju-card"
           style={{
@@ -77,7 +76,6 @@ export class WhyJoinCard extends Component {
           <img src={img1}></img>
           <p>Hey this is text</p>
         </div>
-      </div>
     );
   }
 }
--- src/Components/WhyJoinUs/WhyJoinUs.js
+++ src/Components/WhyJoinUs/WhyJoinUs.js
@@ -6,7 +6,16 @@ import './WhyJoinUs.css'
 export class WhyJoinUs extends Component {
   render() {
     return (
-      <WhyJoinCard />
+      <div>
+      <div className="wju-main-div" id="myDiv">
+
+      <WhyJoinCard x={10} y={20}/>
+      <WhyJoinCard x={40} y={400}/>
+      
+      <WhyJoinCard x={70} y={100}/>
+      </div>
+      </div>
+
     );
   }
 }
