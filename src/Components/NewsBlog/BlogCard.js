f3bb99371629cf20be7579ca4efda165b0103841
Anushka <singhalanushka.1999@gmail.com>
2020-10-07 20:25:43 +0530

    multiple card layout added

--- src/Components/NewsBlog/BlogCard.js
+++ src/Components/NewsBlog/BlogCard.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./BlogCard.css";
 function BlogCard() {
   return (
-    <div>
+      <div>
+    <div className="blog-grid">
         <div className="blog-post">
           <div className="blog-category-container">
             <span>Blog</span>
@@ -13,6 +14,61 @@ function BlogCard() {
               Benefits and Advantages of going Vegetarian
             </h1>
             <h3 className="blog-author">Written by Unknown</h3>
+            <hr/>
+            <p className="blog-p">
+              Thinking about reducing or eliminating animal products from your
+              diet? Whether you want to go vegan or simply add more plant-based
+              foods to your diet, learn how making informed choices about what
+              you eat can improve your health and reduce your risk of certain
+              diseases.
+              
+              
+            </p>
+          </div>
+          <a className="blog-btn_primary" href="#">
+            Read More
+          </a>
+        </div>
+
+
+
+
+        <div className="blog-post">
+          <div className="blog-category-container">
+            <span>Blog</span>
+          </div>
+          <div className="blog-container">
+            <h3 className="blog-date">7 October 2020</h3>
+            <h1 className="blog-heading">
+              Benefits and Advantages of going Vegetarian
+            </h1>
+            <h3 className="blog-author">Written by Unknown</h3>
+            <hr/>
+            <p className="blog-p">
+              Thinking about reducing or eliminating animal products from your
+              diet? 
+            </p>
+          </div>
+          <a className="blog-btn_primary" href="#">
+            Read More
+          </a>
+        </div>
+        </div>
+
+
+
+    <div class="bog-grid">
+        <div className="blog-post">
+          <div className="blog-category-container">
+            <span>Blog</span>
+          </div>
+          <div className="blog-container">
+            <h3 className="blog-date">7 October 2020</h3>
+            <h1 className="blog-heading">
+              Benefits and Advantages of going Vegetarian
+            </h1>
+            <h3 className="blog-author">Written by Unknown</h3>
+            <hr/>
             <p className="blog-p">
               Thinking about reducing or eliminating animal products from your
               diet? Whether you want to go vegan or simply add more plant-based
@@ -25,6 +81,8 @@ function BlogCard() {
             Read More
           </a>
         </div>
+        </div>
+
       </div>
   );
 }
