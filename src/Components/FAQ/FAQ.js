6386dc0ab08799f413b2d7ab747cf6685adc1ef1
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2020-10-07 10:10:52 +0530

    faq hero section added

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import "./FAQ.css";
 
 class FAQ extends Component {
+  
   render() {
     return (
-      <div className="faq-page">
-        <h1>Im from FAQ</h1>
-
-        
+      <div className = "faq-page">
+        <div className = "faq-hero">
+          <div className = "faq-hero-child1">
+            How can we help you ?
+          </div>
+          <div className = "faq-hero-child2">
+            <div className = "faq-hero-text">
+              FAQ
+            </div>
+            <div className = "faq-hero-search">
+              <input type = "text" placeholder = "Enter keywords to search"></input>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
