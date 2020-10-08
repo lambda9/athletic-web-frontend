f7b2c0a70e75c3882b734cbd1f8be3cfaa8cf0b0
Anushka <singhalanushka.1999@gmail.com>
2020-10-08 12:29:45 +0530

    image added and made responsive

--- src/Components/NewsBlog/FullBlogPage.js
+++ src/Components/NewsBlog/FullBlogPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./BlogCard.css"
+import bgImg from "../../Images/im4.jpg";
 function FullBlogPage() {
     return (
         <div>
@@ -12,7 +13,7 @@ function FullBlogPage() {
               Benefits and Advantages of going Vegetarian
             </h1>
             <h3 className="full-blog-author">Written by Unknown</h3>
-            <hr/>
+            <img src={bgImg}/>
             <p className="full-blog-p">
               Thinking about reducing or eliminating animal products from your
               diet? Whether you want to go vegan or simply add more plant-based
