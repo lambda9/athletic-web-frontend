662cd843941573fa2c75a3c10c8e743ef8ff89f7
Devendra Singh <sdev6631@gmail.com>
2020-10-08 09:38:14 +0530

    Tweak CSS of fome enquiry component

--- src/Components/Home/HomePageEnquiry.js
+++ src/Components/Home/HomePageEnquiry.js
@@ -9,7 +9,7 @@ const PRIME_COLOR = "#459fb6";
 const useStyles = (theme) => ({
   root: {
     "& .MuiTextField-root": {
-      margin: '0 auto 1.2rem',
+      margin: "0 auto 1.2rem",
       width: "100%",
     },
   },
@@ -23,9 +23,14 @@ const CssTextField = withStyles({
     "& .MuiInput-underline:after": {
       borderBottomColor: PRIME_COLOR,
     },
+    'label + &': {
+      backgroundColor: 'blue',
+    },
     "& .MuiOutlinedInput-root": {
+      
+
       "& fieldset": {
-        borderColor: "black",
+        borderColor: PRIME_COLOR,
       },
       "&:hover fieldset": {
         color: PRIME_COLOR,
@@ -75,88 +80,88 @@ class HomePageEnquiry extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className='custom'>
-      <div className="div">
-        <div className="div2">
-          <div className="home-enquiry-left-div">
-            <span>I N T E R E S T E D</span>
-            <h2>A N Y Q U E R Y ?</h2>
-            <p>
-              Fill out the form and we will reach you as soon as possible...
-            </p>
-          </div>
-          <div className="home-enquiry-main-div">
-            <form className={classes.root} autoComplete="nope">
-              <CssTextField
-                required
-                type="text"
-                name="name"
-                value={this.state.name}
-                autoComplete="nope"
-                label="Name"
-                variant="outlined"
-                onChange={this.handleChange}
-              />
-              <CssTextField
-                required
-                type="email"
-                value={this.state.email}
-                name="email"
-                autoComplete="nope"
-                label="E-mail"
-                variant="outlined"
-                onChange={this.handleChange}
-              />
-              <CssTextField
-                required
-                value={this.state.phoneNum}
-                type="tel"
-                autoComplete="nope"
-                name="phoneNum"
-                label="Phone Number"
-                variant="outlined"
-                onChange={this.handleChange}
-              />
-              <CssTextField
-                required
-                value={this.state.subject}
-                type="text"
-                name="subject"
-                label="Subject"
-                variant="outlined"
-                onChange={this.handleChange}
-              />
-              <CssTextField
-                required
-                value={this.state.subject}
-                type="textarea"
-                name="subject"
-                multiline
-                rows={6}
-                label="Message"
-                variant="outlined"
-                onChange={this.handleChange}
-              />
+      <div className="custom">
+        <div className="div">
+          <div className="div2">
+            <div className="home-enquiry-left-div">
+              <span>I N T E R E S T E D</span>
+              <h2>A N Y Q U E R Y ?</h2>
+              <p>
+                Fill out the form and we will reach you as soon as possible...
+              </p>
+            </div>
+            <div className="home-enquiry-main-div">
+              <form className={classes.root} autoComplete="nope">
+                <CssTextField
+                  required
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  autoComplete="nope"
+                  label="Name"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                />
+                <CssTextField
+                  required
+                  type="email"
+                  value={this.state.email}
+                  name="email"
+                  autoComplete="nope"
+                  label="E-mail"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                />
+                <CssTextField
+                  required
+                  value={this.state.phoneNum}
+                  type="tel"
+                  autoComplete="nope"
+                  name="phoneNum"
+                  label="Phone Number"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                />
+                <CssTextField
+                  required
+                  value={this.state.subject}
+                  type="text"
+                  name="subject"
+                  label="Subject"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                />
+                <CssTextField
+                  required
+                  value={this.state.subject}
+                  type="textarea"
+                  name="subject"
+                  multiline
+                  rows={6}
+                  label="Message"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                />
 
-              <div className="home-enquiry-btn-container">
-                <button
-                  onClick={this.handleSubmit}
-                  className="button-primary"
-                  type="submit"
-                >
-                  Submit
-                </button>
-                <button
-                  onClick={this.handleReset}
-                  className="button-primary  cancel-btn"
-                >
-                  reset
-                </button>
-              </div>
-            </form>
+                <div className="home-enquiry-btn-container">
+                  <button
+                    onClick={this.handleSubmit}
+                    className="button-primary"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                  <button
+                    onClick={this.handleReset}
+                    className="button-primary  cancel-btn"
+                  >
+                    reset
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-      </div>
-      </div>
+        </div>
       </div>
     );
   }
