7661e0bf5a3409c45a07de344fcb7b1f42b03c18
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2020-10-09 21:27:21 +0530

    user review translate bug fixed

--- src/Components/UserReviews/HomeUserReviews.js
+++ src/Components/UserReviews/HomeUserReviews.js
@@ -3,57 +3,60 @@ import "./UserReviews.css";
 import React, { useState } from "react";
 
 function HomeUserReviews() {
-  const items = [
-    {
-      id: "id-1",
-      altText: "Slide 1",
-      comp: <UserReviewCard />,
-    },
-    {
-      id: "id-2",
-      altText: "Slide 2",
-      comp: <UserReviewCard />,
-    },
-    {
-      id: "id-3",
-      altText: "Slide 3",
-      comp: <UserReviewCard />,
-    },
-  ];
-  
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
+	console.log("rendre");
+	const items = [
+		{
+			id: 1,
+			altText: "Slide 1",
+			comp: <UserReviewCard key={1} />,
+		},
+		{
+			id: 2,
+			altText: "Slide 2",
+			comp: <UserReviewCard key={2} />,
+		},
+		{
+			id: 3,
+			altText: "Slide 3",
+			comp: <UserReviewCard key={3} />,
+		},
+	];
 
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex;
-    setActiveIndex(nextIndex);
-  };
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex;
-    setActiveIndex(nextIndex);
-  };
+	const next = () => {
+		console.log("active index", activeIndex);
+		setActiveIndex((activeIndex) => (activeIndex + 1) % items.length);
+	};
 
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
-  return (
-    <div className="home-review-comp">
-      <div
-        className="home-review-card-slider"
-        style={{
-        transform: `translateX(${-activeIndex*25}vw)`,
-        }}
-      >
-        {items.map((item) => item.comp)}
-      </div>
-      <button onClick={next}>prev</button>
-      <button onClick={previous}>next</button>
-    </div>
-  );
+	const previous = () => {
+		setActiveIndex((activeIndex) => {
+			activeIndex -= 1;
+			if (activeIndex < 0) {
+				activeIndex = items.length - 1;
+			}
+			return activeIndex;
+		});
+	};
+
+	const goToIndex = (newIndex) => {
+		setActiveIndex(newIndex);
+	};
+
+	return (
+		<div className="home-review-comp">
+			<div
+				className="home-review-card-slider"
+				style={{
+					transform: `translateX(${-activeIndex * 50}vw)`,
+				}}
+			>
+				{items.map((item) => item.comp)}
+			</div>
+			<button onClick={previous}>prev</button>
+			<button onClick={next}>next</button>
+		</div>
+	);
 }
 
 export default HomeUserReviews;
