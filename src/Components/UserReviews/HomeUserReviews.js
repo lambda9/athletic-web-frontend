61a8bfa3560897c116d6661f665ca36feba70cb0
Devendra Singh <sdev6631@gmail.com>
2020-10-09 23:44:00 +0530

    Update user review slider

--- src/Components/UserReviews/HomeUserReviews.js
+++ src/Components/UserReviews/HomeUserReviews.js
@@ -2,61 +2,71 @@ import UserReviewCard from "./UserReviewCard";
 import "./UserReviews.css";
 import React, { useState } from "react";
 
+import {
+  FcNext,
+  GrNext,
+  GrPrevious,
+  ImNext,
+  MdNavigateNext,
+} from "react-icons/all";
+
 function HomeUserReviews() {
-	console.log("rendre");
-	const items = [
-		{
-			id: 1,
-			altText: "Slide 1",
-			comp: <UserReviewCard key={1} />,
-		},
-		{
-			id: 2,
-			altText: "Slide 2",
-			comp: <UserReviewCard key={2} />,
-		},
-		{
-			id: 3,
-			altText: "Slide 3",
-			comp: <UserReviewCard key={3} />,
-		},
-	];
+  console.log("rendre");
+  const items = [
+    {
+      id: 1,
+      altText: "Slide 1",
+      comp: <UserReviewCard key={1} />,
+    },
+    {
+      id: 2,
+      altText: "Slide 2",
+      comp: <UserReviewCard key={2} />,
+    },
+    {
+      id: 3,
+      altText: "Slide 3",
+      comp: <UserReviewCard key={3} />,
+    },
+  ];
 
-	const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-	const next = () => {
-		console.log("active index", activeIndex);
-		setActiveIndex((activeIndex) => (activeIndex + 1) % items.length);
-	};
+  const next = () => {
+    console.log("active index", activeIndex);
+    setActiveIndex((activeIndex) => (activeIndex + 1) % items.length);
+  };
 
-	const previous = () => {
-		setActiveIndex((activeIndex) => {
-			activeIndex -= 1;
-			if (activeIndex < 0) {
-				activeIndex = items.length - 1;
-			}
-			return activeIndex;
-		});
-	};
+  const previous = () => {
+    setActiveIndex((activeIndex) => {
+      activeIndex -= 1;
+      if (activeIndex < 0) {
+        activeIndex = items.length - 1;
+      }
+      return activeIndex;
+    });
+  };
 
-	const goToIndex = (newIndex) => {
-		setActiveIndex(newIndex);
-	};
+  const goToIndex = (newIndex) => {
+    setActiveIndex(newIndex);
+  };
 
-	return (
-		<div className="home-review-comp">
-			<div
-				className="home-review-card-slider"
-				style={{
-					transform: `translateX(${-activeIndex * 50}vw)`,
-				}}
-			>
-				{items.map((item) => item.comp)}
-			</div>
-			<button onClick={previous}>prev</button>
-			<button onClick={next}>next</button>
-		</div>
-	);
+  return (
+      <div className='home-review-main-div'>
+    <div className="home-review-comp">
+      <GrPrevious className="sliderBtn prevBtn" onClick={previous} />
+      <GrNext className="sliderBtn nextBtn" onClick={next} />
+      <div
+        className="home-review-card-slider"
+        style={{
+          transform: `translateX(${-activeIndex * 70}vw)`,
+        }}
+      >
+        {items.map((item) => item.comp)}
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeUserReviews;
