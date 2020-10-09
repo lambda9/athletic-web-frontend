d9db8508001ee45bd5a9e127acc82455750e6979
Devendra Singh <sdev6631@gmail.com>
2020-10-09 21:05:28 +0530

    Add carousel in reviews comp

--- src/Components/UserReviews/HomeUserReviews.js
+++ src/Components/UserReviews/HomeUserReviews.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import "./UserReviews.css"
-
+import UserReviewCard from "./UserReviewCard";
+import "./UserReviews.css";
+import React, { useState } from "react";
 
 function HomeUserReviews() {
-    return (
-        <div className='review-home-comp'>
-            
-        </div>
-    )
+  const items = [
+    {
+      id: "id-1",
+      altText: "Slide 1",
+      comp: <UserReviewCard />,
+    },
+    {
+      id: "id-2",
+      altText: "Slide 2",
+      comp: <UserReviewCard />,
+    },
+    {
+      id: "id-3",
+      altText: "Slide 3",
+      comp: <UserReviewCard />,
+    },
+  ];
+  
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <div className="home-review-comp">
+      <div
+        className="home-review-card-slider"
+        style={{
+        transform: `translateX(${-activeIndex*25}vw)`,
+        }}
+      >
+        {items.map((item) => item.comp)}
+      </div>
+      <button onClick={next}>prev</button>
+      <button onClick={previous}>next</button>
+    </div>
+  );
 }
 
-export default HomeUserReviews
+export default HomeUserReviews;
