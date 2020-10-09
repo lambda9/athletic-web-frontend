6c82bfa677c1193c5fe018f3ce2cac51a91cc9be
Devendra Singh <sdev6631@gmail.com>
2020-10-10 02:15:26 +0530

    Update Blog page and card.
    Remove hard coded data.
    Replace repition of code with new Blog card component.
    Refractor unecessary divs.

--- src/Components/NewsBlog/BlogCard.js
+++ src/Components/NewsBlog/BlogCard.js
@@ -1,137 +1,27 @@
 import React from "react";
 import "./BlogCard.css";
-import {Link} from "react-router-dom";
-function BlogCard() {
+import { Link } from "react-router-dom";
+
+
+function BlogCard(props) {
   return (
-      <div className="blog-page">
-        <div className="blog-hero">
-       <h1>NEWS AND FITNESS BLOGS</h1>
-        <h6>News articles and blogs to help you stay motivated throughout your journey!</h6>
-        <p>Discover the latest views and news on health and fitness, nutrition and weight loss that will help fast track you on your way to achieving your fitness goals.</p>
+    <div className="blog-page">
+      <div className="blog-post">
+        <div className="blog-category-container">
+          <span>{props.obj.tag}</span>
         </div>
-        <div className="blog-overlap-card">
-          <div className="blog-overlap-card-box">
-          <h1>NEWS & BLOGS</h1>
-          <p>We understand that for many of our members,
-             exercise plays a huge part of your physical 
-             and mental wellbeing. And we believe that
-              taking care of that right now has never been 
-              more important. To better support you as we
-               get back to the gym, we will be updating our
-                blog regularly to provide you with workout 
-                ideas (for the gym and home), wellbeing and nutrition 
-                content</p>
+        <div className="blog-container">
+          <h3 className="blog-date">{props.obj.date}</h3>
+          <h1 className="blog-heading">{props.obj.heading}</h1>
+          <h3 className="blog-author">{props.obj.author}</h3>
+          <hr />
+          <p className="blog-p">{props.obj.description}</p>
         </div>
-        </div>
-    <div className="blog-grid">
-        <div className="blog-post">
-          <div className="blog-category-container">
-            <span>Blog</span>
-          </div>
-          <div className="blog-container">
-            <h3 className="blog-date">7 October 2020</h3>
-            <h1 className="blog-heading">
-              Benefits and Advantages of going Vegetarian
-            </h1>
-            <h3 className="blog-author">Written by Unknown</h3>
-            <hr/>
-            <p className="blog-p">
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-              
-              
-            </p>
-          </div>
-            <Link to="/fullblog"><button className="blog-btn-primary">Read More</button></Link>
-        </div>
-
-
-
-
-        <div className="blog-post">
-          <div className="blog-category-container">
-            <span>Blog</span>
-          </div>
-          <div className="blog-container">
-            <h3 className="blog-date">7 October 2020</h3>
-            <h1 className="blog-heading">
-              Benefits and Advantages of going Vegetarian
-            </h1>
-            <h3 className="blog-author">Written by Unknown</h3>
-            <hr/>
-            <p className="blog-p">
-              Thinking about reducing or eliminating animal products from your
-              diet? 
-            </p>
-          </div>
-          <Link to="/fullblog"><button className="blog-btn-primary">Read More</button></Link>
-        </div>
-       
-
-
-
-    
-        <div className="blog-post">
-          <div className="blog-category-container">
-            <span>Blog</span>
-          </div>
-          <div className="blog-container">
-            <h3 className="blog-date">7 October 2020</h3>
-            <h1 className="blog-heading">
-              Benefits and Advantages of going Vegetarian
-            </h1>
-            <h3 className="blog-author">Written by Unknown</h3>
-            <hr/>
-            <p className="blog-p">
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-            </p>
-          </div>
-          <Link to="/fullblog"><button className="blog-btn-primary">Read More</button></Link>
-         
-        </div>
-        <div className="blog-post">
-          <div className="blog-category-container">
-            <span>Blog</span>
-          </div>
-          <div className="blog-container">
-            <h3 className="blog-date">7 October 2020</h3>
-            <h1 className="blog-heading">
-              Benefits and Advantages of going Vegetarian
-            </h1>
-            <h3 className="blog-author">Written by Unknown</h3>
-            <hr/>
-            <p className="blog-p">
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-              
-              
-            </p>
-          </div>
-            <Link to="/fullblog"><button className="blog-btn-primary">Read More</button></Link>
-        </div>
-        </div>
-        </div>
-        
+        <Link to="/fullblog">
+          <button className="blog-btn-primary">Read More</button>
+        </Link>
+      </div>
+    </div>
   );
 }
 export default BlogCard;
--- src/Components/NewsBlog/FullBlogPage.js
+++ src/Components/NewsBlog/FullBlogPage.js
@@ -1,57 +1,58 @@
-import React from 'react'
-import "./BlogCard.css"
+import React from "react";
+import "./BlogCard.css";
 import bgImg from "../../Images/im4.jpg";
 function FullBlogPage() {
-    return (
-        <div>
-             <div>
-    <div className="full-blog-grid">
-        <div className="full-blog-post">
-          <div className="full-blog-container">
-            <h3 className="full-blog-date">7 October 2020</h3>
-            <h1 className="full-blog-heading">
-              Benefits and Advantages of going Vegetarian
-            </h1>
-            <h3 className="full-blog-author">Written by Unknown</h3>
-            <img src={bgImg}/>
-            <p className="full-blog-p">
-              Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.  Thinking about reducing or eliminating animal products from your
-              diet? Whether you want to go vegan or simply add more plant-based
-              foods to your diet, learn how making informed choices about what
-              you eat can improve your health and reduce your risk of certain
-              diseases.
-              
-            </p>
+  return (
+    <div>
+      <div>
+        <div className="full-blog-grid">
+          <div className="full-blog-post">
+            <div className="full-blog-container">
+              <h3 className="full-blog-date">7 October 2020</h3>
+              <h1 className="full-blog-heading">
+                Benefits and Advantages of going Vegetarian
+              </h1>
+              <h3 className="full-blog-author">Written by Unknown</h3>
+              <img src={bgImg} />
+              <p className="full-blog-p">
+                Thinking about reducing or eliminating animal products from your
+                diet? Whether you want to go vegan or simply add more
+                plant-based foods to your diet, learn how making informed
+                choices about what you eat can improve your health and reduce
+                your risk of certain diseases. Thinking about reducing or
+                eliminating animal products from your diet? Whether you want to
+                go vegan or simply add more plant-based foods to your diet,
+                learn how making informed choices about what you eat can improve
+                your health and reduce your risk of certain diseases. Thinking
+                about reducing or eliminating animal products from your diet?
+                Whether you want to go vegan or simply add more plant-based
+                foods to your diet, learn how making informed choices about what
+                you eat can improve your health and reduce your risk of certain
+                diseases. Thinking about reducing or eliminating animal products
+                from your diet? Whether you want to go vegan or simply add more
+                plant-based foods to your diet, learn how making informed
+                choices about what you eat can improve your health and reduce
+                your risk of certain diseases. Thinking about reducing or
+                eliminating animal products from your diet? Whether you want to
+                go vegan or simply add more plant-based foods to your diet,
+                learn how making informed choices about what you eat can improve
+                your health and reduce your risk of certain diseases. Thinking
+                about reducing or eliminating animal products from your diet?
+                Whether you want to go vegan or simply add more plant-based
+                foods to your diet, learn how making informed choices about what
+                you eat can improve your health and reduce your risk of certain
+                diseases. Thinking about reducing or eliminating animal products
+                from your diet? Whether you want to go vegan or simply add more
+                plant-based foods to your diet, learn how making informed
+                choices about what you eat can improve your health and reduce
+                your risk of certain diseases.
+              </p>
+            </div>
           </div>
         </div>
-        </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default FullBlogPage
+export default FullBlogPage;
