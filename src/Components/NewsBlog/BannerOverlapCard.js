84872b09605ebb96bd306782d11bddf8d5e6f5fa
Devendra Singh <sdev6631@gmail.com>
2020-10-10 15:41:48 +0530

    Update banner overlap card

--- src/Components/NewsBlog/BannerOverlapCard.js
+++ src/Components/NewsBlog/BannerOverlapCard.js
@@ -4,9 +4,8 @@ function BannerOverlapCard() {
   return (
     <div className="blog-overlap-card">
       <div className="blog-overlap-card-headings">
-        <span id="overlap-card-head-news">news</span>
-
-        <span id="overlap-card-head-blog">blog</span>
+      <span id="overlap-card-head-news">news &</span>
+      <span id="overlap-card-head-blog">blogs</span>
       </div>
       <p>
         We understand that for many of our members, exercise plays a huge part
