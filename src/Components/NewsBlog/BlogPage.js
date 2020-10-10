f8969ecafc69d431d24d943a96d3dc7aad2ee96b
Devendra Singh <sdev6631@gmail.com>
2020-10-10 15:12:09 +0530

    Update responsive design of complete blogs page and blogs card

--- src/Components/NewsBlog/BlogPage.js
+++ src/Components/NewsBlog/BlogPage.js
@@ -8,10 +8,17 @@ function BlogPage() {
   return (
     <div className="blog-page">
       <BlogPageBanner />
-      <div className="blog-grid">
-        {BlogDt.map((item) => {
-          return <BlogCard key={item.id} obj={item} />;
-        })}
+      <div className="blog-page-cards-row">
+        <div className="blog-page-cards-col">
+          {BlogDt.map((item) => {
+            return <BlogCard key={item.id} obj={item} />;
+          })}
+        </div>
+        <div className="blog-page-cards-col">
+          {BlogDt.map((item) => {
+            return <BlogCard key={item.id} obj={item} />;
+          })}
+        </div>
       </div>
     </div>
   );
