a3084002bfd040d5505355875eb227f6e49cc82c
Devendra Singh <sdev6631@gmail.com>
2020-10-11 18:58:01 +0530

    Update design of slider

--- src/Components/OurPlans/PlanDurationSlider.js
+++ src/Components/OurPlans/PlanDurationSlider.js
@@ -2,24 +2,60 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Slider from "@material-ui/core/Slider";
 
+const PRIMARY_RED = "#c13434";
+const PRIMARY_COLOR = "#1ca9c9";
 const useStyles = makeStyles({
   root: {
-    width: "200",
-    backgroundColor: "red",
+    width: "300",
+    "& .MuiSlider-root": {
+      width: "700px",
+      height: "20px",
+      color: "whtite"
+    },
+    "&  .MuiSlider-markLabel": {
+      color: 'white',
+      fontSize: "18px",
+      fontWeight: "bold",
+    },
+    "& .MuiSlider-rail": {
+      backgroundColor: "white",
+      color: "white",
+      opacity: "0.9",
+      height: "5px",
+    },
+    "& .MuiSlider-track": {
+      backgroundColor: PRIMARY_RED,
+      height: "5px",
+    },
+    "& .MuiSlider-thumb": {
+      width: "15px",
+      height: "15px",
+      backgroundColor: PRIMARY_RED,
+    },
+    "& .MuiSlider-mark": {
+      width: "3px",
+      height: "3px",
+    },
+
+    "& PrivateValueLabel-circle-5": {
+      width: "36px",
+      height: "36px",
+      backgroundColor: "yellow",
+    },
   },
 });
 const marks = [
   {
     value: 1,
-    label: "Month",
+    label: "Monthly",
   },
   {
-    value: 4,
-    label: "Quaterly",
+    value: 3,
+    label: "Quarterly",
   },
   {
     value: 6,
-    label: "Half Yearly",
+    label: "Half-Yearly",
   },
   {
     value: 12,
@@ -31,14 +67,16 @@ function valuetext(value) {
 }
 
 function valueLabelFormat(value) {
-  return marks.findIndex((mark) => mark.value === value) + 1;
+  return "value Months";
 }
 function PlanDurationSlider() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Slider
-        defaultValue={6}
+        defaultValue={20}
+        min={1}
+        max={12}
         valueLabelFormat={valueLabelFormat}
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider-restrict"
