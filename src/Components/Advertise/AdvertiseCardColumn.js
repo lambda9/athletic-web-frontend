4754e55323dfc2b0feed05b5a6faf3f51d9b50dc
Anushka <singhalanushka.1999@gmail.com>
2020-10-12 22:40:17 +0530

    removed gradient and icon color changed

--- src/Components/Advertise/AdvertiseCardColumn.js
+++ src/Components/Advertise/AdvertiseCardColumn.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./Advertise.css";
+import { IconContext } from "react-icons/lib";
 const CardColumn = ({ title, icon, info }) => {
   return (
     <div className="advertise-card-item-container">
-        {icon}
+      <IconContext.Provider value={{ className: "advertise-card-column-icon"}}>{icon}</IconContext.Provider>
       <h4>{title}</h4>
       <p>{info}</p>
     </div>
