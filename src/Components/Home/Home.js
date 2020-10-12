11b3d0188a639a6a14ec04db52eca5e60cc8a071
Devendra Singh <45001741+devspartan@users.noreply.github.com>
2020-10-12 21:01:20 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,6 @@ import HomePageFAQ from '../FAQ/HomePageFAQ'
 import WhyJoinUs from '../WhyJoinUs/WhyJoinUs'
 import OurPlans from "../OurPlans/OurPlans";
 import HomeUserReviews from "../UserReviews/HomeUserReviews";
-import HomeSubscribe from "../SubscribeNewsLetter/HomeSubscribe";
 import FadingCarousel from "./FadingCarousel";
 
 
@@ -18,7 +17,6 @@ function Home() {
 			<HomePageEnquiry />
 			<HomePageFAQ />
 			<HomeUserReviews />
-			<HomeSubscribe />
 		</div>
 	);
 }
