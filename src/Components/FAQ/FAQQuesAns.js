953b5ccdb62e11ab76d9c06015abad171327a863
Devendra Singh <sdev6631@gmail.com>
2020-10-13 12:50:08 +0530

    Update FAQ page accordion

--- src/Components/FAQ/FAQQuesAns.js
+++ src/Components/FAQ/FAQQuesAns.js
@@ -1,135 +1,209 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./FAQ.css";
-import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
-const useStyles = makeStyles((theme) => ({
-  accordion: {
-    width: '100%',
-    boxShadow: 'none',
-    paddingBottom: '1em'
-  },
-  question: {
-    fontFamily: '"Monteserrat", sans-serif',
-    fontWeight: 'bold',
-  },
-  details: {
-    borderBottom: '0.15em solid var(--accentColor)'
-  },
-  answer: {
-    fontFamily: '"Monteserrat", sans-serif',
-    textAlign: 'Left'
-  }
-}));
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const arrQuesAns = [
   {
+    id: "faq_1",
+    expanded: "panel1",
     type: "type1",
     ques: "Do you have lockers?",
-    ans: "Absolutely, and a lot of them. No need to bring a lock, we operate on a code combination setup."
+    ans:
+      "Absolutely, and a lot of them. No need to bring a lock, we operate on a code combination setup.",
   },
   {
+    id: "faq_2",
+    expanded: "panel2",
     type: "type1",
     ques: "Do you have water or should I bring my own?",
-    ans: "All our branches are zero single-use plastic free and we provide reusable stainless bottles to borrow for your session. If you’d prefer to bring your own bottle, we also have water stations available."
+    ans:
+      "All our branches are zero single-use plastic free and we provide reusable stainless bottles to borrow for your session. If you’d prefer to bring your own bottle, we also have water stations available.",
   },
   {
+    id: "faq_3",
+    expanded: "panel3",
     type: "type1",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_4",
+    expanded: "panel4",
     type: "type1",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_5",
+    expanded: "panel5",
     type: "type1",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_6",
+    expanded: "panel6",
     type: "type2",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_7",
+    expanded: "panel7",
     type: "type2",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_8",
+    expanded: "panel8",
     type: "type2",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_9",
+    expanded: "panel9",
     type: "type2",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_10",
+    expanded: "panel10",
     type: "type3",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_11",
+    expanded: "panel11",
     type: "type3",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
   {
+    id: "faq_12",
+    expanded: "panel12",
     type: "type3",
     ques: "lorem ipsum ?",
-    ans: "lorem ipsum"
+    ans: "lorem ipsum",
   },
 ];
 
-const QuesAns = (props) => {
-
-  function typeFilter (e) {
+function QuesAns(props) {
+  const typeFilter = (e) => {
     return e.type === props.type;
-  }
+  };
+
+  const [width, setwidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    console.log(window.innerWidth);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const handleWindowResize = () => {
+    setwidth(window.innerWidth);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: "100%",
+      margin: "1rem auto",
+      "& .MuiAccordion-rounded": {
+        margin: "0.8rem auto",
+      },
+      "& .Mui-expanded": {},
+      "& .MuiAccordionDetails-root": {
+        padding: "0 10px 12px 10px",
+      },
+      "& .MuiAccordionSummary-content.Mui-expanded ": {
+        margin: "10px 0",
+      },
+      "& .MuiAccordionSummary-content": {
+        margin: "10px 0",
+      },
+      "& .MuiAccordionSummary-root": {
+        padding: "0 10px",
+      },
+      "& .MuiAccordionSummary-root.Mui-expanded": {
+        minHeight: "0",
+      },
+      
+    },
+    acc: {
+      transition: "all linear 0.2s",
+      backgroundColor: "transparent",
+    },
+    head: {
+      "& .Mui-expanded": {},
+    },
+    heading: {
+      fontSize: width < 650 ? "16px" : "20px",
+      transition: "all linear 0.2s",
+      flexBasis: "100%",
+      margin: "0rem auto",
+      textAlign: "left",
+    },
+    details: {
+      fontSize: width < 650 ? "12px" : "16px",
+      textAlign: "left",
+    },
+  }));
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   const classes = useStyles();
+
   const typeArr = arrQuesAns.filter(typeFilter);
 
   return (
-    <div className = "faq-qa-main">
-      <div className = "faq-type-head">
-        <div id = "type-head">{props.type}</div>
+    <div className="faq-qa-main">
+      <div className="faq-type-head">
+        <div id="type-head">{props.type}</div>
       </div>
-      <div className = "faq-accordion">
-      {
-        typeArr.map( (item) => {
-          return (
-
-            <Accordion
-              className={classes.accordion}
-            >
-              <AccordionSummary
-                className={classes.summary}
-                expandIcon={<ExpandMoreIcon />}
+      <div className="faq-accordion">
+        <div className={classes.root}>
+          {typeArr.map((item) => {
+            return (
+              <Accordion
+                expanded={expanded === item.expanded}
+                onChange={handleChange(item.expanded)}
+                className={classes.acc}
               >
-                <Typography className={classes.question}> 
-                  {item.ques} 
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails
-                className={classes.details}
-              >
-                <Typography className={classes.answer}>
-                  {item.ans}
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })
-      }  
+                <AccordionSummary
+                  key={item.id}
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={item.expanded + "bh-content"}
+                  id={item.expanded + "bh-header"}
+                  className={classes.head}
+                >
+                  <Typography className={classes.heading}>
+                    {item.ques}{" "}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography className={classes.details}>
+                    {item.ans}{" "}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
