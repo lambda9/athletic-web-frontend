111ce0ba3d9a27602de7e24ee319994c897e3107
Devendra Singh <sdev6631@gmail.com>
2020-10-15 01:51:02 +0530

    Update scale for table text

--- src/Components/BMI/BMITable.js
+++ src/Components/BMI/BMITable.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function BMITable(props) {
-    const{ bmi} = props
-    var clr1 = (bmi < 18.5 && bmi > 0) ? 'red' : 'black'
-    var clr2 = (18.5 <= bmi && bmi <= 24.9) ? "green" : "black"
-    var clr3 = (24.9 < bmi && bmi <= 29.9) ? 'red' : "black"
-    var clr4 = (29.9 < bmi) ? 'red' : 'black'
+  const { bmi } = props;
+  var clr1 = bmi < 18.5 && bmi > 0 ? "red" : "black";
+  var clr2 = 18.5 <= bmi && bmi <= 24.9 ? "green" : "black";
+  var clr3 = 24.9 < bmi && bmi <= 29.9 ? "red" : "black";
+  var clr4 = 29.9 < bmi ? "red" : "black";
 
+  const getStyle = (num, clr) => {
+    if (num == 1 || num == 3 || num == 4) {
+      return { color: clr, transform: `scale(${clr === 'red' ? 1.3 : 1})` };
+    } else if (num == 2) {
+      return { color: clr, transform: `scale(${clr === 'green' ? 1.3 : 1})` };
+    }
+  };
   return (
     <div className="bmi-content-left-div">
       <h4>BMI CHART</h4>
@@ -16,20 +23,28 @@ function BMITable(props) {
           <th id="no-right-border">Wight Status</th>
         </tr>
         <tr>
-          <td style={{color: clr1}}>Below 18.5 {props.bmi}</td>
-          <td style={{color: clr1}} id="no-right-border">underweight</td>
+          <td style={getStyle(1, clr1)}>Below 18.5</td>
+          <td style={getStyle(1, clr1)} id="no-right-border">
+            underweight
+          </td>
         </tr>
         <tr>
-          <td style={{color: clr2}}>18.5 - 24.9</td>
-          <td style={{color: clr2}} id="no-right-border">healthy</td>
+          <td style={getStyle(2, clr2)}>18.5 - 24.9</td>
+          <td style={getStyle(2, clr2)} id="no-right-border">
+            healthy
+          </td>
         </tr>
         <tr>
-          <td style={{color: clr3}}>25.0 - 29.9</td>
-          <td style={{color: clr3}} id="no-right-border">overweight</td>
+          <td style={getStyle(3, clr3)}>25.0 - 29.9</td>
+          <td style={getStyle(3, clr3)} id="no-right-border">
+            overweight
+          </td>
         </tr>
-        <tr id="no-bottom-border">
-          <td style={{color: clr4}}>30.0 - above</td>
-          <td style={{color: clr4}} id="no-right-border">obese</td>
+        <tr id="no-bottom-border" >
+          <td style={getStyle(4, clr4)}>30.0 - above</td>
+          <td style={getStyle(4, clr4)} id="no-right-border">
+            obese
+          </td>
         </tr>
       </table>
     </div>
