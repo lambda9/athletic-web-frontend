f050b386404eda1d996a5f92bc5c5caaf1916ea5
Devendra Singh <sdev6631@gmail.com>
2020-10-15 23:00:15 +0530

    Remove syntax error from footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,55 +8,49 @@ import SocialMedia from "./SocialMedia";
 import GetApp from "./GetApp";
 
 class Footer extends Component {
-
-   links1 = [
+  links1 = [
     ["/", "Home"],
-    ['blog', 'Blog'],
+    ["blog", "Blog"],
     ["gallery", "Gallery"],
     ["programs", "Programs"],
     ["membership", "Membership"],
-    
+  ];
 
   links2 = [
     ["faq", "FAQ"],
     ["aboutUs", "About us"],
-    ["news","Latest News"],
+    ["news", "Latest News"],
     ["contactUs", "Contact Us"],
-    ['advertise', "Advertise with us"]
+    ["advertise", "Advertise with us"],
   ];
 
-  render () {
-
+  render() {
     return (
-      
-      <div className = "footer-component">
-        <div className = "footer-text">
-          WORK HARD . PLAY HARD
-        </div>
-        <div className = "footer-main">
-          <div className = "footer-child child1">
-            <Footerhead head = "Newsletter" />
-            <div className = "footer-child-content">
+      <div className="footer-component">
+        <div className="footer-text">WORK HARD . PLAY HARD</div>
+        <div className="footer-main">
+          <div className="footer-child child1">
+            <Footerhead head="Newsletter" />
+            <div className="footer-child-content">
               <Newsletter />
-
             </div>
           </div>
-          <div className = "footer-child child2">
-            <Footerhead head = "Quick links" />
-            <div className = "footer-child-content">
-              <div className = "footer-quicklink-group">
-                <Quicklinks group = {this.links1} />
-                <Quicklinks group = {this.links2} />
+          <div className="footer-child child2">
+            <Footerhead head="Quick links" />
+            <div className="footer-child-content">
+              <div className="footer-quicklink-group">
+                <Quicklinks group={this.links1} />
+                <Quicklinks group={this.links2} />
               </div>
             </div>
           </div>
-          <div className = "footer-child child3">
-            <Footerhead head = "Stay connected" />
-            <div className = "footer-child-content">
+          <div className="footer-child child3">
+            <Footerhead head="Stay connected" />
+            <div className="footer-child-content">
               <SocialMedia />
             </div>
-            <Footerhead head = "Get our app" />
-            <div className = "footer-child-content">
+            <Footerhead head="Get our app" />
+            <div className="footer-child-content">
               <GetApp />
             </div>
           </div>
@@ -68,11 +62,8 @@ class Footer extends Component {
           <div>All rights reserved</div>
         </div>
       </div>
-
     );
-
   }
-
 }
 
 export default Footer;
