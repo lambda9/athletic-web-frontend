e9373139f2d190995f7b210bd3d1516aab1147fe
Anushka <singhalanushka.1999@gmail.com>
2020-10-18 19:14:56 +0530

    refactored code

--- src/Components/Membership/MembershipIcon.js
+++ src/Components/Membership/MembershipIcon.js
@@ -6,60 +6,39 @@ import i2 from "../../Images/MembershipIcons/i2.png";
 import i3 from "../../Images/MembershipIcons/i3.png";
 
 function MembershipIcon() {
+    const cards=[
+        {
+            img:i1,
+            heading:"High-Class Equipments",
+            info:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at."
+
+        },
+        {
+            img:i2,
+            heading:"Easy on your pocket",
+            info:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at."
+
+        },
+        {
+            img:i3,
+            heading:"Flexible Timings",
+            info:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at."
+
+        }
+    ]
     return (
         <div className="membership-icon-background">
             <h1>Reasons to become a member at the Atheletic Gym</h1>
-             <div className="membership-icon-container">
-        <div className="membership-icon-card">
-            <div className="face face1">
-                <div className="membership-icon-card-content">
-                    <img src={i1}/>
-                    <h3>High-Class Equipments</h3>
-                </div>
-            </div>
-            <div className="face face2">
-            <div className="membership-icon-card-content">
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        
-                </div>
-            </div>
-        </div>
-        
-        <div className="membership-icon-card">
-
-            <div className="face face1">
-            <div className="membership-icon-card-content">
-
-                    <img src={i2}/>
-                    <h3>Easy on your Pocket</h3>
-                </div>
-            </div>
-            <div className="face face2">
-            <div className="membership-icon-card-content">
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        
-                </div>
-            </div>
-        </div>
-        <div className="membership-icon-card">
-
-            <div className="face face1">
-            <div className="membership-icon-card-content">
-
-                    <img src={i3}/>
-                    <h3>Flexible Timings</h3>
-                </div>
-            </div>
-            <div className="face face2">
-            <div className="membership-icon-card-content">
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        
-                </div>
-            </div>
-        </div>
+            <div className="membership-icon-container">
+                return(
+                    {cards.map((items,index)=>
+                    <MembershipIconCard
+                    key={items.index}
+                    img={items.img}
+                    heading={items.heading}
+                    info={items.info}
+                    />)}
+                )
     </div>
         </div>
     )
