f4460bc16a918e00d28f896830af8057287183f5
bob <vikas.singh.iitr@gmail.com>
2020-10-21 00:28:15 +0530

    Update design of franshise page

--- src/Components/Franchise/FranchiseBanner.js
+++ src/Components/Franchise/FranchiseBanner.js
@@ -7,7 +7,11 @@ function FranchiseBanner() {
       <div className="franchise-header-text">Franchise</div>
       <div className="franchise-header-div-sub-heading">
         <h1>OWN YOUR OWN FRANCHISE TODAY</h1>
-        <span>The fitness industry is constantly growing which means success for our franchisees. It also gives us a meaningful purpose to change the lives in our communities for the better.</span>
+        <span>
+          The fitness industry is constantly growing which means success for our
+          franchises. It also gives us a meaningful purpose to change the lives
+          in our communities for the better.
+        </span>
       </div>
     </div>
   );
