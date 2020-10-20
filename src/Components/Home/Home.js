fbf731276a44dc755d7e5540f1cd8808caf42568
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2020-10-20 23:39:07 +0530

    old features file removed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,6 @@ import HomeUserReviews from "./UserReviews/HomeUserReviews";
 import FadingCarousel from "./FadingCarousel/FadingCarousel";
 import Covid from "./Covid/Covid";
 import BMI from "./BMI/BMI";
-import Features from "./WhatWeProvide/Features";
 import Results from "./WhatWeProvide/Results";
 import Programs from "./Programs/Programs";
 import { features } from "./WhatWeProvide/featuresData";
