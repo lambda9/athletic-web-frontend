0942846e18b6f06dea37a94d30638203ebb54d91
bob <vikas.singh.iitr@gmail.com>
2020-10-22 00:52:04 +0530

    Add news letter component on home page and update responsive design

--- src/Components/Home/NewsLetter/NewsLetter.js
+++ src/Components/Home/NewsLetter/NewsLetter.js
@@ -1,54 +1,53 @@
+import { NavigateBeforeSharp } from "@material-ui/icons";
 import React, { useState } from "react";
 import "./NewsLetter.css";
 
 function NewsLetter() {
+  const newsLetterDiv = [
+    {
+      id: 1,
+      tag: "blog",
+      date: "7th October 2020",
+      heading: "Benefits and Advantages of going Vegetarian",
+      author: "Andy Circus",
+      description:
+        "Thinking about reducing or eliminating animal products from your diet? Whether you want to go vegan or simply add more plant-based foods to your diet, learn how making informed choices about what you eat can improve your health and reduce your risk of certain diseases. Thinking about reducing or eliminating animal products from your diet? Whether you want to go vegan or simply add more plant-based foods to your diet, learn how making informed choices about what you eat can improve your health and reduce your risk of certain diseases.",
+    },
+    {
+      id: 2,
+      tag: "blog",
+      date: "7th October 2020",
+      heading: "Benefits and Advantages of going Vegetarian",
+      author: "Andy Circus",
+      description:
+        "Thinking about reducing or eliminating animal products from your diet?choices about what you eat can improve your health and reduce your risk of certain diseases. Thinking about reducing or eliminating animal products from your diet? Whether you want to go vegan or simply add more plant-based foods to your diet, learn how making informed choices about what you eat can improve your health and reduce your risk of certain diseases.",
+    },
+    {
+      id: 3,
+      tag: "news",
+      date: "8th October 2020",
+      heading: "Benefits and Advantages of going Vegetarian",
+      author: "Andy Circus",
+      description:
+        "Thinking about reducing or eliminating animal products from your diet? Whether you want to go vegan or simply add more plant-based foods to your diet, learn how making informed choices about what you eat can improve your health and reduce your risk of certain diseases. Thinking about reducing or eliminating animal products from your diet? Whether you want to go vegan or simply add more plant-based foods to your diet, learn how making informed choices about what you eat can improve your health and reduce your risk of certain diseases.",
+    },
+  ];
   return (
     <div className="news-letter-main-component">
-      <h4>NEWS LETTER</h4>
       <h1>LATEST NEWS</h1>
       <div className="news-letter-card">
-        <div>
-          <a href="/blog">NEWS</a>
-          <h2>BEST TRAININGS</h2>
-          <author>by: SPARTAN</author>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            <a className="button-primary " href="/blog">
-              Read More
-            </a>
-          </span>
-        </div>
-        <div>
-          <a href="/blog">NEWS</a>
-          <h2>BEST TRAININGS</h2>
-          <author>by: SPARTAN</author>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            <a className="button-primary " href="/blog">
-              Read More
-            </a>
-          </span>
-        </div>
-        <div>
-          <a href="/blog">NEWS</a>
-          <h2>BEST TRAININGS</h2>
-          <author>by: SPARTAN</author>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            <a className="button-primary " href="/blog">
-              Read More
-            </a>
-          </span>
-        </div>
+        {newsLetterDiv.map((item) => {
+          return (
+            <div>
+              <a href="/blog">{item.tag}</a>
+              <h5>{item.heading}</h5>
+              <author>{item.author}</author>
+              <date>{item.date}</date>
+              <span>{item.description.slice(0,150)}<a href="/blog">...Read More</a></span>
+            </div>
+          );
+        })}
+        
       </div>
     </div>
   );
