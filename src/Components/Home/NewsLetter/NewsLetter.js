c846339f04bbd3b9daa26735e1396a15bb91c55f
bob <vikas.singh.iitr@gmail.com>
2020-10-21 22:11:10 +0530

    Improved news-letter

--- src/Components/Home/NewsLetter/NewsLetter.js
+++ src/Components/Home/NewsLetter/NewsLetter.js
@@ -15,10 +15,10 @@ function NewsLetter() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.<button className="button-primary">Read More</button>
+            aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            <a className="button-primary " href="/blog">
+              Read More
+            </a>
           </span>
         </div>
         <div>
@@ -29,10 +29,10 @@ function NewsLetter() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.<button className="button-primary">Read More</button>
+            aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            <a className="button-primary " href="/blog">
+              Read More
+            </a>
           </span>
         </div>
         <div>
@@ -43,10 +43,10 @@ function NewsLetter() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.<button className="button-primary">Read More</button>
+            aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            <a className="button-primary " href="/blog">
+              Read More
+            </a>
           </span>
         </div>
       </div>
