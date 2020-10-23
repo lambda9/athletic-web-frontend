82b8e3e0bf9fc46a17c07143f99693a92cb44413
Devendra Singh <sdev6631@gmail.com>
2020-10-23 23:47:21 +0530

    Update social media links with actual links

--- src/Components/Footer/SocialMedia.js
+++ src/Components/Footer/SocialMedia.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FaInstagram, BiPhoneCall, SiGooglemaps, FaFacebookF } from "react-icons/all";
+import {
+  FaInstagram,
+  BiPhoneCall,
+  SiGooglemaps,
+  FaFacebookF,
+} from "react-icons/all";
 
 export class SocialMedia extends Component {
-  
   render() {
-
     return (
-
       <div className="footer-social-media">
         <div className="footer-social-media-cont">
-          <Link to='/'>
+          <a
+            href="https://www.facebook.com/Athletic-Gym-105083634244606"
+            target="_blank"
+          >
             <FaFacebookF />
-          </Link>
-          <Link to='/'>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/theathleticgym/">
             <FaInstagram />
-          </Link>
-          <Link to='/'>
+          </a>
+          <Link href="/https://www.youtube.com/channel/UCaLrziwKCIUS6PWlwu1Y8Hw/featured">
             <BiPhoneCall />
           </Link>
-          <Link to='/'>
+          <Link to="/">
             <SiGooglemaps />
           </Link>
         </div>
       </div>
-
     );
-
   }
-
 }
 
 export default SocialMedia;
