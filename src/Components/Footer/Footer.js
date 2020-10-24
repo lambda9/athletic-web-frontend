565a451c37763b061db4ab1b984f67508c27fec1
bob <vikas.singh.iitr@gmail.com>
2020-10-25 00:01:54 +0530

    removed footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,65 +6,70 @@ import Newsletter from "./Newsletter";
 import { FaCopyright } from "react-icons/all";
 import SocialMedia from "./SocialMedia";
 import GetApp from "./GetApp";
+import { useLocation } from "react-router-dom";
 
-class Footer extends Component {
-	links1 = [
-		["/", "Home"],
-		["blog", "Blog"],
-		["gallery", "Gallery"],
-		["programs", "Programs"],
-		["membership", "Membership"],
-		["pt", "PT"],
-	];
+function Footer() {
+  const links1 = [
+    ["/", "Home"],
+    ["blog", "Blog"],
+    ["gallery", "Gallery"],
+    ["programs", "Programs"],
+    ["membership", "Membership"],
+    ["pt", "PT"],
+  ];
 
-	links2 = [
-		["faq", "FAQ"],
-		["aboutUs", "About us"],
-		["news", "Latest News"],
-		["contactUs", "Contact Us"],
-		["advertise", "Advertise with us"],
-	];
+  const links2 = [
+    ["faq", "FAQ"],
+    ["aboutUs", "About us"],
+    ["news", "Latest News"],
+    ["contactUs", "Contact Us"],
+    ["advertise", "Advertise with us"],
+  ];
 
-	render() {
-		return (
-			<footer className="footer-component">
-				<div className="footer-text">WORK HARD . PLAY HARD</div>
-				<div className="footer-main">
-					<div className="footer-child">
-						<Footerhead head="Newsletter" />
-						<div className="footer-child-content">
-							<Newsletter />
-						</div>
-					</div>
-					<div className="footer-child">
-						<Footerhead head="Quick links" />
-						<div className="footer-child-content">
-							<div className="footer-quicklink-group">
-								<Quicklinks group={this.links1} />
-								<Quicklinks group={this.links2} />
-							</div>
-						</div>
-					</div>
-					<div className="footer-child">
-						<Footerhead head="Stay connected" />
-						<div className="footer-child-content">
-							<SocialMedia />
-						</div>
-						<Footerhead head="Get our app" />
-						<div className="footer-child-content">
-							<GetApp />
-						</div>
-					</div>
-				</div>
-				<div className="footer-copyright">
-					<div>
-						<FaCopyright /> Athletic gym and fitness center
-					</div>
-					<div>All rights reserved</div>
-				</div>
-			</footer>
-		);
-	}
+  const location = useLocation();
+
+  return (
+    <div>
+      {location.pathname == "/login" ? null : (
+        <footer className="footer-component">
+          <div className="footer-text">WORK HARD . PLAY HARD</div>
+          <div className="footer-main">
+            <div className="footer-child">
+              <Footerhead head="Newsletter" />
+              <div className="footer-child-content">
+                <Newsletter />
+              </div>
+            </div>
+            <div className="footer-child">
+              <Footerhead head="Quick links" />
+              <div className="footer-child-content">
+                <div className="footer-quicklink-group">
+                  <Quicklinks group={links1} />
+                  <Quicklinks group={links2} />
+                </div>
+              </div>
+            </div>
+            <div className="footer-child">
+              <Footerhead head="Stay connected" />
+              <div className="footer-child-content">
+                <SocialMedia />
+              </div>
+              <Footerhead head="Get our app" />
+              <div className="footer-child-content">
+                <GetApp />
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div>
+              <FaCopyright /> Athletic gym and fitness center
+            </div>
+            <div>All rights reserved</div>
+          </div>
+        </footer>
+      )}
+    </div>
+  );
 }
 
 export default Footer;
